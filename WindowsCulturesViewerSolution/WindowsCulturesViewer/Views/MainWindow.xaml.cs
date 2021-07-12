d4a717ec60a49866b3015863308ad590bbe1735c
Владимир Ермолаев <Ermolaev.vova@hotmail.com>
2021-07-12 22:27:23 +0500

    Init

---
+++ WindowsCulturesViewerSolution/WindowsCulturesViewer/Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace WindowsCulturesViewer.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
